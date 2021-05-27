3393198c259de3146c6f89d3d36f91f3627a88ac
jung341416 <jung341416@gmail.com>
2021-05-26 20:25:19 -0400

    practice

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,58 @@
-exports.toRoman = function(num) {
+let modernRomanArr = [
+{"M": 1000},
+{"CM": 900},
+{"D": 500},
+{"CD": 400},
+{"C": 100},
+{"XC": 90},
+{"L": 50},
+{"XL": 40},
+{"X": 10},
+{"IX": 9},
+{"V": 5},
+{"IV": 4},
+{"I": 1}
+]
+let lazyRomanArr = [
+  {"M": 1000},
+  {"D": 500},
+  {"C": 100},
+  {"L": 50},
+  {"X": 10},
+  {"V": 5},
+  {"I": 1}
+  ]
+exports.lazyRoman = function(num) {
+// create output string 
+  return convertLazyRoman(num,lazyRomanArr);
 
 };
+exports.newRoman = function(num) {
+  return convertLazyRoman(num,modernRomanArr)
+  
+};
+function convertLazyRoman(num, data) {
+    // create output string 
+  let output = '';
+  let inputNum = num;
+  
+  //iterate through romanArr and if the inputnum is less than each romanArr
+  //and divide by it and add each iterate letter to output string
+  
+  for (let obj of data) {
+    for(let key in obj) {
+      let arabicNum = obj[key];
+      let romanNum = key;
+      let divideInput = Math.floor(inputNum/ arabicNum);
+      if(divideInput >= 1) {
+        inputNum = inputNum % arabicNum;
+        output += romanNum.repeat(divideInput);
+      }
+    }
+  }
+  
+  //return output string
+  return output;
+  
+  };
+  
--- js/romanNumeralsSpec.js
+++ js/romanNumeralsSpec.js
@@ -1,5 +1,19 @@
 var rn = require("./romanNumerals");
 
-console.log(rn.toRoman(1) === 'I');
-console.log(rn.toRoman(3) === 'III');
-console.log(rn.toRoman(4) === 'IV');
+console.log(rn.lazyRoman(3) === 'III');
+console.log(rn.lazyRoman(1) === 'I');
+console.log(rn.lazyRoman(4) === 'IIII');
+console.log(rn.lazyRoman(300) === 'CCC');
+console.log(rn.lazyRoman(123) === 'CXXIII');
+console.log(rn.lazyRoman(2400) === 'MMCCCC');
+console.log(rn.lazyRoman(939) === 'DCCCCXXXVIIII');
+
+// modern test cases
+console.log(rn.newRoman(1) === 'I');
+console.log(rn.newRoman(3) === 'III');
+console.log(rn.newRoman(4) === 'IV');
+console.log(rn.newRoman(300) === 'CCC');
+console.log(rn.newRoman(123) === 'CXXIII');
+console.log(rn.newRoman(2400) === 'MMCD');
+console.log(rn.newRoman(939) === 'CMXXXIX');
+console.log(rn.newRoman(594) === 'DXCIV');
